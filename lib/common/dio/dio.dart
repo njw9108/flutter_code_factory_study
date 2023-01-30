a909e63016abfb0981db7041758b0ed16f0743c8
JINWOO <njw9108@naver.com>
2023-01-30 12:18:20 +0900

    feat: interceptor OnError 작성

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -15,6 +15,7 @@ class CustomInterceptor extends Interceptor {
   @override
   void onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
+    print('[REQ] [${options.method}] [${options.uri}]');
     //요청값 변경
     if (options.headers['accessToken'] == 'true') {
       //헤더 삭제
@@ -27,7 +28,6 @@ class CustomInterceptor extends Interceptor {
       });
     }
 
-
     if (options.headers['refreshToken'] == 'true') {
       //헤더 삭제
       options.headers.remove('refreshToken');
@@ -38,20 +38,72 @@ class CustomInterceptor extends Interceptor {
         'authorization': 'Bearer $token',
       });
     }
+
+    //요청을 보냄
     return super.onRequest(options, handler);
   }
 
   //2. 응답을 받을때
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    // TODO: implement onResponse
     super.onResponse(response, handler);
   }
 
   //3. 에러가 났을때
   @override
-  void onError(DioError err, ErrorInterceptorHandler handler) {
-    // TODO: implement onError
-    super.onError(err, handler);
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    //401에러가 났을때(status code)
+    //토큰 재발급 받는 시도를 하고 토큰이 재발급 되면
+    //다시 새로운 토큰으로 요청을 한다.
+    print('[ERROR] [${err.requestOptions.method}] [${err.requestOptions.uri}]');
+
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+
+    //리프레쉬 토큰이 아예 없으면 에러를 던진다.
+    if (refreshToken == null) {
+      //에러를 발생시킨다. 항상 handler.reject를 사용한다(dio 문법)
+      return handler.reject(err);
+    }
+
+    final isStatus401 = err.response?.statusCode == 401;
+    //토큰을 발급 받으려다 에러가 난 경우
+    final isPathRefresh = err.requestOptions.path == '/auth/token';
+
+    try {
+      if (isStatus401 && !isPathRefresh) {
+        final dio = Dio();
+        final resp = await dio.post(
+          'http://$ip/auth/token',
+          options: Options(
+            headers: {
+              'authorization': 'Bearer $refreshToken',
+            },
+          ),
+        );
+
+        final accessToken = resp.data['accessToken'];
+
+        final options = err.requestOptions;
+
+        //새로운 토큰을 넣어줌
+        options.headers.addAll({
+          'authorization': 'Bearer $accessToken',
+        });
+
+        //토큰 변경
+        await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+        //요청 재전송(토큰만 바꿔서)
+        final response = await dio.fetch(options);
+
+        //에러없이 요청을 끝낼수 있다.
+        return handler.resolve(response);
+      }
+    } on DioError catch (e) {
+      //그대로 에러를 반환
+      return handler.reject(e);
+    }
+
+    return super.onError(err, handler);
   }
 }
