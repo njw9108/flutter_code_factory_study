d10e020d4deb28d1cb1e79709a4104604c970f87
JINWOO <njw9108@naver.com>
2023-02-05 01:59:13 +0900

    feat: profile 정보 UI 추가

--- lib/user/view/profile_screen.dart
+++ lib/user/view/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:code_factory/user/model/user_model.dart';
 import 'package:code_factory/user/provider/user_me_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,13 +8,45 @@ class ProfileScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          ref.read(userMeProvider.notifier).logout();
-        },
-        child: const Text('로그아웃'),
-      ),
+    final state = ref.watch(userMeProvider);
+    final pState = state as UserModel;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 15,
+              backgroundImage: NetworkImage(pState.imageUrl),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              pState.username,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            )
+          ],
+        ),
+        const Spacer(),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              ref.read(userMeProvider.notifier).logout();
+            },
+            child: const Text('로그아웃'),
+          ),
+        ),
+        const Spacer(),
+      ],
     );
   }
 }
