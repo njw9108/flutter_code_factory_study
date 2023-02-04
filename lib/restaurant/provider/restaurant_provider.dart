dbfb96607e0e9054c6fe94c6424b54e6ce572afa
JINWOO <njw9108@naver.com>
2023-02-04 16:18:39 +0900

    feat: 없는 데이터 캐시하기

--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -1,9 +1,9 @@
 import 'package:code_factory/common/model/cursor_pagination.dart';
-import 'package:code_factory/common/model/pagination_params.dart';
 import 'package:code_factory/common/provider/pagination_provider.dart';
 import 'package:code_factory/restaurant/model/restaurant_model.dart';
 import 'package:code_factory/restaurant/repository/restaurant_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:collection/collection.dart';
 
 final restaurantDetailProvider = Provider.family<RestaurantModel?, String>(
   (ref, id) {
@@ -11,7 +11,7 @@ final restaurantDetailProvider = Provider.family<RestaurantModel?, String>(
     if (state is! CursorPagination) {
       return null;
     }
-    return state.data.firstWhere((element) => element.id == id);
+    return state.data.firstWhereOrNull((element) => element.id == id);
   },
 );
 
@@ -47,10 +47,22 @@ class RestaurantStateNotifier
 
     final pState = state as CursorPagination;
     final resp = await repository.getRestaurantDetail(id: id);
-    state = pState.copyWith(
-      data: pState.data
-          .map<RestaurantModel>((e) => e.id == id ? resp : e)
-          .toList(),
-    );
+
+    if (pState.data.where((element) => element.id == id).isEmpty) {
+      state = pState.copyWith(
+        data: <RestaurantModel>[
+          ...pState.data,
+          resp,
+        ],
+      );
+    } else {
+      state = pState.copyWith(
+        data: pState.data
+            .map<RestaurantModel>(
+              (e) => e.id == id ? resp : e,
+            )
+            .toList(),
+      );
+    }
   }
 }
