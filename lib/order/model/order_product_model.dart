219047151821e3b8864fe2850ca5c93862781e1a
JINWOO <njw9108@naver.com>
2023-02-04 23:26:22 +0900

    feat: order repository 생성

--- lib/order/model/order_product_model.dart
+++ lib/order/model/order_product_model.dart
@@ -1,4 +1,6 @@
+import 'package:code_factory/common/model/model_with_id.dart';
 import 'package:code_factory/common/utils/data_utils.dart';
+import 'package:code_factory/restaurant/model/restaurant_model.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'order_product_model.g.dart';
@@ -26,4 +28,42 @@ class OrderProductModel {
       _$OrderProductModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$OrderProductModelToJson(this);
+}
+
+@JsonSerializable()
+class OrderProductAndCountModel {
+  final OrderProductModel product;
+  final int count;
+
+  OrderProductAndCountModel({
+    required this.product,
+    required this.count,
+  });
+
+  factory OrderProductAndCountModel.fromJson(Map<String, dynamic> json) =>
+      _$OrderProductAndCountModelFromJson(json);
+}
+
+@JsonSerializable()
+class OrderModel implements IModelWithId {
+  @override
+  final String id;
+  final List<OrderProductAndCountModel> products;
+  final int totalPrice;
+  final RestaurantModel restaurant;
+  @JsonKey(
+    fromJson: DataUtils.stringToDateTime,
+  )
+  final DateTime createAt;
+
+  OrderModel({
+    required this.id,
+    required this.products,
+    required this.totalPrice,
+    required this.restaurant,
+    required this.createAt,
+  });
+
+  factory OrderModel.fromJson(Map<String, dynamic> json) =>
+      _$OrderModelFromJson(json);
 }
