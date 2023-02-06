a17474c9809935ee02a25c1e3bf534f17ffc3caf
JINWOO <njw9108@naver.com>
2023-02-06 23:49:33 +0900

    feat: cursorpagination 클래스 제너릭 추가

--- lib/common/model/cursor_pagination_model.dart
+++ lib/common/model/cursor_pagination_model.dart
@@ -46,7 +46,7 @@ class CursorPaginationMeta {
 }
 
 //새로고침
-class CursorPaginationRefetching extends CursorPagination {
+class CursorPaginationRefetching<T> extends CursorPagination<T> {
   CursorPaginationRefetching({
     required super.meta,
     required super.data,
@@ -54,7 +54,7 @@ class CursorPaginationRefetching extends CursorPagination {
 }
 
 //리스트의 맨 아래로 내려서 추가 데이터를 요청하는 중
-class CursorPaginationFetchingMore extends CursorPagination {
+class CursorPaginationFetchingMore<T> extends CursorPagination<T> {
   CursorPaginationFetchingMore({
     required super.meta,
     required super.data,
